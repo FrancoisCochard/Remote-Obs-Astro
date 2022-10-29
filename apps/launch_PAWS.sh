9323e876b04801cb2b8437c37d135655103e5b7b
Francois Cochard <francois.cochard@shelyak.com>
2022-10-29 19:19:42 +0200

    Je mémorise les dernières modifs (à la marge) pour basculer sur la branche Dev.

--- apps/launch_PAWS.sh
+++ apps/launch_PAWS.sh
@@ -1,3 +1,3 @@
 PRJ_DIR=/home/observatoire/Obs-St-Pancrasse/INDI-Python/Remote-Obs-Astro
-export PAWS=/home/observatoire/Obs-St-Pancrasse/INDI-Python/PAWS
+export PAWS=/home/observatoire/Obs-St-Pancrasse/INDI-Python/PAWS/
 python /home/observatoire/Obs-St-Pancrasse/INDI-Python/PAWS/app.py
---
+++ pdt1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Basic function
+
+pdt1() {
+   bash ./apps/launch_indi_simu.sh
+}
+
+pdt1
