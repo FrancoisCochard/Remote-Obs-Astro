59d1fbd421968e8a8e862677738883f03547fd36
Francois Cochard <francois.cochard@shelyak.com>
2023-12-09 09:18:05 +0100

    Le script TestFlow.py marche bien (toujours en mode rudimentaire). Je peux lancer plusieurs séquences d'observation (approche multi-programme) et j'ai une commande STOP qui arrête le process à la fin de l'étape en cours (ex : acquisition ou guidage)

--- ObservingProcess/TestFlow.py
+++ ObservingProcess/TestFlow.py
@@ -33,7 +33,11 @@ def F5():
     print("F5 - fin")
     return "OK"
 
-seq1 = {"Pointer":F1, "Centrer":F1, "Guider":F2, "Acquisition":F3, "Flat":F4, "Dark":F5}
+sequence = {
+'main':{"Pointage":F1, "Centrage":F1, "Guidage":F2, "Acquisition":F3, "Flat":F4, "Dark":F5},
+'seq1':{"Pointer":F1, "Centrer":F2, "Acquisition":F3},
+'seq2':{"Guider":F2, "Acquisition":F3, "Flat":F4, "Dark":F5}
+}
 
 class ProcessObs:
     
@@ -41,45 +45,72 @@ class ProcessObs:
         self.seq = seq
         self.err = "OK"
         self.ix = 'none'
-        self.X = threading.Thread(target = self.thr_func)
+        self.stop = False
+        self.X = threading.Thread(target = self.observing_process_thread)
+        self.X.start()
     
-    def thr_func(self):
+    def observing_process_thread(self):
         for step in self.seq:
+            if self.stop == True:
+                print("Process interrompu")
+                break
             if self.err == "OK":
                 self.ix = step
                 self.err = self.seq[step]()
             else :
                 print("BUG")
+                break
     
-    def run(self):
-        if self.X.is_alive() == False:
-            self.X.start()
-            return "Processus démarré"
-        else :
-            return "Le process tourne déjà"
-    
-    def state(self):
-        if self.X.is_alive():
-            return self.X.is_alive(), str(self.ix)
+    def stop_process(self, message_stop):
+        self.stop = True
+        print("Interruption du process : ", message_stop)
+
+def ObsProcessRun(process):
+    global A
+    if 'A' in globals() and A.X.is_alive() == True:
+        return "Le process tourne déjà"
+    else:
+        if process in sequence.keys():
+            A = ProcessObs(sequence[process])
+            reply = "Processus démarré : " + process
+            return reply
         else:
-            return self.X.is_alive(), "Not running"
+            return "Processus inconnu"
+
+def ObsProcessStop(message_stop):
+    global A
+    if 'A' in globals() and A.X.is_alive() == True:
+        A.stop_process(message_stop)
+        return True
+    else:
+        print("The process is not running")
+        return False
+        
+def ObsState():
+    global A
+    if 'A' in globals():
+        if A.X.is_alive():
+            return A.X.is_alive(), str(A.ix)
+        else:
+            return False, "Not running"
+    else:
+        return False, "Not running"
 
 app = FastAPI()
 
-@app.get('/state')
+@app.get("/state")
 async def get_state():
-    return A.state()
+    return ObsState()
 
-@app.get('/run')
-async def get_state():
-    return A.run()
+@app.get("/run/{process}")
+async def get_run(process):
+    return ObsProcessRun(process)
+
+@app.get("/stop/{message_stop}")
+async def get_stop(message_stop):
+    ObsProcessStop(message_stop)
+    return True
 
 if __name__ == "__main__":
     print("On démarre")
-    A = ProcessObs(seq1)
-    # ~ A.run()
-    uvicorn.run(app, host="0.0.0.0", port=1235)
-
-    # ~ for i in range(10) :
-        # ~ print("Etape en cours : ", A.state())
-        # ~ time.sleep(2)
+    uvicorn.run("TestFlow:app", host="0.0.0.0", port=1235, reload=True)
