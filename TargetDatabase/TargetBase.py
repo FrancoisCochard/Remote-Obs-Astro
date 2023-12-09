781ef02dd8ac7793e45fe40194783a95ad061166
Francois Cochard <francois.cochard@shelyak.com>
2023-12-09 20:18:00 +0100

    Je reviens sur la (petite) base de données d'étoiles - la base SQlite. J'ajoute une commande qui permet de retourner la liste des index TargetID, de manière à pouvoir extraire une étoile au hasard dans la base. C'est la fonction query_List_TargetID. C'est un peu tordu de ne sortir qu'une liste simple, mais ce soir ça marche.

--- TargetDatabase/TargetBase.py
+++ TargetDatabase/TargetBase.py
@@ -7,69 +7,83 @@ F. Cochard, 04/10/2022
 
 import sqlite3
 import os
+import random
 from Target.Target import Target
 
 class TargetBase():
 	
-	DataBase = os.path.expanduser("~/Obs-St-Pancrasse/INDI-Python/Remote-Obs-Astro/TargetDatabase/TargetBase.db")
+    DataBase = os.path.expanduser("~/Obs-St-Pancrasse/INDI-Python/Remote-Obs-Astro/TargetDatabase/TargetBase.db")
 
-	def query_get(self, Name):
-		tdb = sqlite3.connect(self.DataBase)
-		cursor = tdb.cursor()
-		T = Target()
-		request = "SELECT TargetID FROM Alias WHERE AliasName = '" + Name + "' COLLATE NOCASE"
-		result = cursor.execute(request)
-		res = result.fetchall()
-		if (len(res) == 0): # No result
-			cursor.close()
-			return(0, T)
-		if (len(res) == 1): # One result, OK
-			TargetID = str(res[0][0])
-			request = "SELECT * FROM Targets WHERE TargetID = '" + TargetID + "'"
-			result = cursor.execute(request)
-			res = result.fetchone()
-			T.MyName = Name
-			T.SimbadName = res[1]
-			T.RA = res[2]
-			T.DEC = res[3]
-			T.Magnitude = res[4]
-			T.SpectralType = res[5]
-			T.Extended = "TBD"
-			T.Moving = "TBD"
-			T.Select = res[8]
-			T.Priority = res[9]
-			cursor.close()
-			return(1, T)
-	
-	def query_add(self, TargetData):
-		tdb = sqlite3.connect(self.DataBase)
-		cursor = tdb.cursor()
-		T = Target()
-		Name = TargetData.SimbadName # To check if the star is already in the local database
-		RA = TargetData.RA
-		DEC = TargetData.DEC
-		Mag = TargetData.Magnitude
-		Sp = TargetData.SpectralType
-		request = "SELECT TargetID FROM Targets WHERE SimbadName = '" + Name + "' COLLATE NOCASE"
-		result = cursor.execute(request)
-		res = result.fetchall()
-		if (len(res) == 0): # The alias is not known in local database
-			print("The Object is unknown in the local database. We add it.")
-			request = '''INSERT INTO Targets
-				(SimbadName, RA, DEC, Magnitude, SpectralType) 
-				VALUES 
-				(?, ?, ?, ?, ?)
-				'''
-			result = cursor.execute(request, (Name, RA, DEC, Mag, Sp))
-			tdb.commit()
-		print("We add an alias name.")
-		request = "SELECT TargetID FROM Targets WHERE SimbadName = '" + Name + "' COLLATE NOCASE"
-		result = cursor.execute(request)
-		res = result.fetchall()
-		TargetID = str(res[0][0])
-		AliasName = TargetData.MyName
-		request = "INSERT INTO Alias (TargetID, AliasName, Simbad) VALUES (" + TargetID + ", '" + AliasName + "', 0)"
-		result = cursor.execute(request)
-		tdb.commit()
-		cursor.close()
-		print(f"The new name is added in the local database (ID : {TargetID}).\n------\n")
+    def query_get(self, Name):
+        tdb = sqlite3.connect(self.DataBase)
+        cursor = tdb.cursor()
+        T = Target()
+        request = "SELECT TargetID FROM Alias WHERE AliasName = '" + Name + "' COLLATE NOCASE"
+        result = cursor.execute(request)
+        res = result.fetchall()
+        if (len(res) == 0): # No result
+            cursor.close()
+            return(0, T)
+        if (len(res) == 1): # One result, OK
+            TargetID = str(res[0][0])
+            request = "SELECT * FROM Targets WHERE TargetID = '" + TargetID + "'"
+            result = cursor.execute(request)
+            res = result.fetchone()
+            T.MyName = Name
+            T.SimbadName = res[1]
+            T.RA = res[2]
+            T.DEC = res[3]
+            T.Magnitude = res[4]
+            T.SpectralType = res[5]
+            T.Extended = "TBD"
+            T.Moving = "TBD"
+            T.Select = res[8]
+            T.Priority = res[9]
+            cursor.close()
+            return(1, T)
+
+    def query_add(self, TargetData):
+        tdb = sqlite3.connect(self.DataBase)    
+        cursor = tdb.cursor()
+        T = Target()
+        Name = TargetData.SimbadName # To check if the star is already in the local database
+        RA = TargetData.RA
+        DEC = TargetData.DEC
+        Mag = TargetData.Magnitude
+        Sp = TargetData.SpectralType
+        request = "SELECT TargetID FROM Targets WHERE SimbadName = '" + Name + "' COLLATE NOCASE"
+        result = cursor.execute(request)
+        res = result.fetchall()
+        if (len(res) == 0): # The alias is not known in local database
+            print("The Object is unknown in the local database. We add it.")
+            request = '''INSERT INTO Targets
+                (SimbadName, RA, DEC, Magnitude, SpectralType) 
+                VALUES 
+                (?, ?, ?, ?, ?)
+                '''
+            result = cursor.execute(request, (Name, RA, DEC, Mag, Sp))
+            tdb.commit()
+        print("We add an alias name.")
+        request = "SELECT TargetID FROM Targets WHERE SimbadName = '" + Name + "' COLLATE NOCASE"
+        result = cursor.execute(request)
+        res = result.fetchall()
+        TargetID = str(res[0][0])
+        AliasName = TargetData.MyName
+        request = "INSERT INTO Alias (TargetID, AliasName, Simbad) VALUES (" + TargetID + ", '" + AliasName + "', 0)"
+        result = cursor.execute(request)
+        tdb.commit()
+        cursor.close()
+        print(f"The new name is added in the local database (ID : {TargetID}).\n------\n")
+
+    def query_List_TargetID(self):
+        # 09/12/2023 : pour accéder à une étoile de la base... je retourne la liste des index disponibles
+        tdb = sqlite3.connect(self.DataBase)
+        cursor = tdb.cursor()
+        result = cursor.execute("select TargetID from Targets")
+        rows = result.fetchall()
+        result_list = []
+        for a in rows:
+            result_list.append(a[0])
+        tdb.close()
+        return result_list
+
