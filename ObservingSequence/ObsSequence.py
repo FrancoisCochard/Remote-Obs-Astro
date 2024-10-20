36df4f4f424d11870debea326474af9b9a1a8600
Francois Cochard <francois.cochard@shelyak.com>
2024-10-20 10:56:50 +0200

    J'ai installé et appliqué le formattage "Black" dans VSCode, sur 2 fichiers pour le moment. So far so good.

--- ObservingSequence/ObsSequence.py
+++ ObservingSequence/ObsSequence.py
@@ -6,7 +6,7 @@
 # V 0.01 : 09/12/2023 - F. Cochard - version initiale, qui marche à peu près.
 # V 0.02 : 31/12/2023 - F. Cochard - on dispose maintenant de l'initialisation des devices Indi.
 # V 0.03 : 07/01/2024 - F. Cochard - j'ajoute le système de Logging (récupéré de ce qu'on avait fait avec Etienne)
-# 
+#
 # L'idée est de pouvoir déclencher une séquence d'observation à partir de l'observatoire, par une API Rest.
 # J'ai le choix de la séquence d'observation.
 # Pour le moment, je ne passe aucun paramètre, mais je sens que ça pourrait se faire plutôt par un fichier centralisé (ou un json).
@@ -15,13 +15,13 @@
 # J'ai 3 commandes API : run pour lancer, state pour demander dans quel étape est le système, et stop pour interrompre (si la pluie arrive par exemple)
 # L'interruption n'est effective qu'à la fin de la fonction Haut Niveau en cours.
 # La classe ProcessObs est instanciée à chaque démarrage d'une séquence d'observation. C'est la variable A (on pourrait trouver un meilleur nom :>)
-# 
-# 
-# 
-# 
-# 
-# 
-# 
+#
+#
+#
+#
+#
+#
+#
 # -----------------------------------------
 
 import time
@@ -32,56 +32,75 @@ import importlib
 import yaml
 from IPX800_V4.IPX800_V4 import StartAllPSU, StopAllPSU
 from utils.LoggingUtils import initLogger
-import ObservingSequence.ObservingOperations as SeqOp # SeqOp means 'Sequence Operation', or a basic operation in an observing sequence.
+import ObservingSequence.ObservingOperations as SeqOp  # SeqOp means 'Sequence Operation', or a basic operation in an observing sequence.
 
-logger = initLogger('obs')
+logger = initLogger("obs")
 
 sequence = {
-'main':{"Pointage":SeqOp.F1, "Centrage":SeqOp.F1, "Guidage":SeqOp.F3, "Acquisition":SeqOp.TakeOneImage, "Flat":SeqOp.F4, "Dark":SeqOp.F5},
-'basic':{"Pointage":SeqOp.F1},
-'BeUVEX':{"Pointage":SeqOp.RawPointingTelescope,
-          "Centrage":SeqOp.PrecisePointingTelescope,
-          "Guidage":SeqOp.ActivateAutoguiding,
-          "Acquisition":SeqOp.TakeTargetSpectraSeries,
-          "StopGuiding":SeqOp.StopAutoguiding,
-          "Calibration":SeqOp.TakeCalibSpectraSeries,
-          "Flat":SeqOp.TakeFlatSpectraSeries,
-          "Dark":SeqOp.TakeDarkSeries,
-          "Bias":SeqOp.TakeBiasSeries,
-          "CreateObsFile":SeqOp.CreateObservationFile},
-'seq1':{"Pointer":SeqOp.F1, "Centrer":SeqOp.TakeOneImage, "Acquisition":SeqOp.F3},
-'seq2':{"Guider":SeqOp.F1, "Acquisition":SeqOp.TakeOneImage, "Flat":SeqOp.F4, "Dark":SeqOp.F5}
+    "main": {
+        "Pointage": SeqOp.F1,
+        "Centrage": SeqOp.F1,
+        "Guidage": SeqOp.F3,
+        "Acquisition": SeqOp.TakeOneImage,
+        "Flat": SeqOp.F4,
+        "Dark": SeqOp.F5,
+    },
+    "basic": {"Pointage": SeqOp.F1},
+    "BeUVEX": {
+        "Pointage": SeqOp.RawPointingTelescope,
+        "Centrage": SeqOp.PrecisePointingTelescope,
+        "Guidage": SeqOp.ActivateAutoguiding,
+        "Acquisition": SeqOp.TakeTargetSpectraSeries,
+        "StopGuiding": SeqOp.StopAutoguiding,
+        "Calibration": SeqOp.TakeCalibSpectraSeries,
+        "Flat": SeqOp.TakeFlatSpectraSeries,
+        "Dark": SeqOp.TakeDarkSeries,
+        "Bias": SeqOp.TakeBiasSeries,
+        "CreateObsFile": SeqOp.CreateObservationFile,
+    },
+    "seq1": {
+        "Pointer": SeqOp.F1,
+        "Centrer": SeqOp.TakeOneImage,
+        "Acquisition": SeqOp.F3,
+    },
+    "seq2": {
+        "Guider": SeqOp.F1,
+        "Acquisition": SeqOp.TakeOneImage,
+        "Flat": SeqOp.F4,
+        "Dark": SeqOp.F5,
+    },
 }
 
 # ObsData is the dictionnary that contains all the data required to run and record an observation.
 # This ObsData is given as the single parameter to all the operations of an observation.
-ObsData = {'Observatory': {}, 'Devices': {}, 'Observation': {}}
-ObsData['Observatory'] = {
-    'site': 'St-Pancrasse',
-    'observer': 'F. Cochard',
-    'instrument': 'UVEX 600'
+ObsData = {"Observatory": {}, "Devices": {}, "Observation": {}}
+ObsData["Observatory"] = {
+    "site": "St-Pancrasse",
+    "observer": "F. Cochard",
+    "instrument": "UVEX 600",
     # A mettre dans un fichier de config...
 }
-ObsData['Observation'] = {
-    'nb': 3,
-    'exptime': 5,
-    'x1': 100,
-    'y1': 250,
-    'x2': 1500,
-    'y2': 1400,
-    'seq': 'BeUVEX',
-    'obsfilename': 'toto.yaml'
+ObsData["Observation"] = {
+    "nb": 3,
+    "exptime": 5,
+    "x1": 100,
+    "y1": 250,
+    "x2": 1500,
+    "y2": 1400,
+    "seq": "BeUVEX",
+    "obsfilename": "toto.yaml",
 }
 
+
 class ProcessObs:
-    
+
     def __init__(self, obs_data):
         self.obs_data = obs_data
-        self.seq = sequence[obs_data['Observation']['seq']]
+        self.seq = sequence[obs_data["Observation"]["seq"]]
         self.err = "OK"
-        self.ix = 'none'
+        self.ix = "none"
         self.stop = False
-        self.X = threading.Thread(target = self.observing_process_thread)
+        self.X = threading.Thread(target=self.observing_process_thread)
         self.X.start()
 
     def observing_process_thread(self):
@@ -92,41 +111,46 @@ class ProcessObs:
             if self.err == "OK":
                 self.ix = step
                 self.err = self.seq[step](self.obs_data)
-            else :
+            else:
                 print("BUG")
                 break
-    
+
     def stop_process(self, message_stop):
         self.stop = True
         print("Interruption du process : ", message_stop)
 
+
 def ObsProcessRun():
     global A
-    if 'A' in globals() and A.X.is_alive() == True:
+    if "A" in globals() and A.X.is_alive() == True:
         return "Le process tourne déjà"
     else:
-        seq = ObsData['Observation']['seq']
+        seq = ObsData["Observation"]["seq"]
         if seq in sequence.keys():
             logger.info(f"We run the process '{seq}'")
             A = ProcessObs(ObsData)
             reply = "Processus démarré : " + seq
             return reply
         else:
-            logger.warning(f"Process '{seq}' requested, but cannot be ran : does not exist")
+            logger.warning(
+                f"Process '{seq}' requested, but cannot be ran : does not exist"
+            )
             return "Processus inconnu"
+
 
 def ObsProcessStop(message_stop):
     global A
-    if 'A' in globals() and A.X.is_alive() == True:
+    if "A" in globals() and A.X.is_alive() == True:
         A.stop_process(message_stop)
         return True
     else:
         print("The process is not running")
         return False
-        
+
+
 def ObsState():
     global A
-    if 'A' in globals():
+    if "A" in globals():
         if A.X.is_alive():
             return A.X.is_alive(), str(A.ix)
         else:
@@ -134,61 +158,74 @@ def ObsState():
     else:
         return False, "Not running"
 
+
 def ReadYamlConfig(Fichier):
-    with open(Fichier, 'r') as file:
+    with open(Fichier, "r") as file:
         config_data = yaml.safe_load(file)
     return config_data
+
 
 def CreatIndiDevices(config_data):
     Devices = {}
     DevicesList = list(config_data)
     for i in DevicesList:
-        ModuleName = config_data[i]['module']
+        ModuleName = config_data[i]["module"]
         try:
-            moduleDir= config_data[i]['module_dir']
-            device_module = importlib.import_module(moduleDir + '.' + ModuleName)
-            class_name = config_data[i]['class_name']
-            Devices[i] = getattr(device_module, class_name)(config=config_data[i], logger=logger, connect_on_create=False)
-            message = "Device creation OK: " + ModuleName + " / " + class_name # + " // " + Devices[i]
+            moduleDir = config_data[i]["module_dir"]
+            device_module = importlib.import_module(moduleDir + "." + ModuleName)
+            class_name = config_data[i]["class_name"]
+            Devices[i] = getattr(device_module, class_name)(
+                config=config_data[i], logger=logger, connect_on_create=False
+            )
+            message = (
+                "Device creation OK: " + ModuleName + " / " + class_name
+            )  # + " // " + Devices[i]
             logger.info(message)
         except:
             message = "Exception during device creation: " + ModuleName
             logger.error(message)
-    ObsData['Devices'] = Devices
+    ObsData["Devices"] = Devices
     print("Dev : ", Devices)
     return True
 
+
 def ConnectDevices():
-    for dev in list(ObsData['Devices']):
-        device = ObsData['Devices'][dev]
+    for dev in list(ObsData["Devices"]):
+        device = ObsData["Devices"][dev]
         print("CONNEXION  :", device)
-        device.connect()        
+        device.connect()
         message = "Device Connexion OK: " + str(device)
         logger.info(message)
 
+
 def DisconnectDevices():
-    for dev in list(ObsData['Devices']):
-        device = ObsData['Devices'][dev]
+    for dev in list(ObsData["Devices"]):
+        device = ObsData["Devices"][dev]
         device.disconnect_device()
         message = "Device disonnexion: " + str(device)
         logger.info(message)
 
+
 app = FastAPI()
+
 
 @app.get("/state")
 async def get_state():
     return ObsState()
 
+
 @app.get("/run")
 async def get_run():
-    print("Ici : ", ObsData['Observation']['seq'])
+    print("Ici : ", ObsData["Observation"]["seq"])
     ObsProcessRun()
     return True
+
 
 @app.get("/stop/{message_stop}")
 async def get_stop(message_stop):
     ObsProcessStop(message_stop)
     return True
+
 
 @app.get("/startupdevices")
 async def get_startupdevices():
@@ -198,19 +235,22 @@ async def get_startupdevices():
     # Pour mémoire (feb 2024), je peux ajouter ici la lecture du fichier de config de PHD2... à réfléchir
     return True
 
+
 @app.get("/disconnectdevices")
 async def get_disconnectdevices():
     DisconnectDevices()
     return True
 
+
 @app.get("/takeimage")
 async def get_takeimage():
-    print("Et là...", ObsData['Devices'])
-    camera = ObsData['Devices']['science_camera']
+    print("Et là...", ObsData["Devices"])
+    camera = ObsData["Devices"]["science_camera"]
     print("data : ", camera)
     print("Jusque ici OK ")
     SeqOp.TakeImage(ObsData, "TOTO.fits")
     return True
+
 
 @app.get("/startupallpsu")
 async def get_startupallpsus():
@@ -218,10 +258,12 @@ async def get_startupallpsus():
     StartAllPSU()
     return True
 
+
 @app.get("/stopallpsu")
 async def get_StopAllPSU():
     StopAllPSU()
     return True
+
 
 if __name__ == "__main__":
     logger.info("Starting of ObservingSequence FastAPI server")
--- ObservingSequence/ObservingOperations.py
+++ ObservingSequence/ObservingOperations.py
@@ -4,121 +4,139 @@
 # ObservingOperations.py
 # V 0.01 - Jan 7th, 2024 - F. Cochard
 # This script contains the operations used by the Observing sequences. These are the elementary bricks of the observations.
-# 
-# 
-# 
-# 
-# 
-# 
+#
+#
+#
+#
+#
+#
 # -----------------------------------------
 import time
 from astropy import units as u
 from astropy.coordinates import SkyCoord
 
+
 def RawPointingTelescope(ObsData):
     print("Pointing the telescope (raw) - wait 3s")
-    mount = ObsData['Devices']['mount']
+    mount = ObsData["Devices"]["mount"]
     # mount.unpark() # à confirmer...
     # mount.slew_to_coord_and_stop() # Je dois encore donner les coordonnées
     # time.sleep(3)
     # c = SkyCoord("12h56m02s	 +38d19m06s", frame='icrs') # vEGA ?
-    c = SkyCoord("01h13m43s	 +07d34m31s", frame='icrs')
+    c = SkyCoord("01h13m43s	 +07d34m31s", frame="icrs")
     print("BEFORE SLEWING --------------------------")
     c_true = mount.get_current_coordinates()
-    print(f"Coordinates are now: ra:{c_true.ra.to(u.hourangle)}, dec:{c_true.dec.to(u.degree)}")
+    print(
+        f"Coordinates are now: ra:{c_true.ra.to(u.hourangle)}, dec:{c_true.dec.to(u.degree)}"
+    )
     mount.slew_to_coord_and_track(c)
     print("After SLEWING --------------------------")
     c_true = mount.get_current_coordinates()
-    print(f"Coordinates are now: ra:{c_true.ra.to(u.hourangle)}, dec:{c_true.dec.to(u.degree)}")
+    print(
+        f"Coordinates are now: ra:{c_true.ra.to(u.hourangle)}, dec:{c_true.dec.to(u.degree)}"
+    )
     print("Le télescope est maintenant sur la cible")
-    return 'OK'
+    return "OK"
+
 
 def CheckFocusing(ObsData):
     print("Check telescope focusing - wait 3s")
     time.sleep(3)
-    return 'OK'
+    return "OK"
+
 
 def DefineSlitPosition(ObsData):
     print("Define slit position - wait 3s")
     time.sleep(3)
-    x1 =1000
-    x2 =1000
+    x1 = 1000
+    x2 = 1000
     y1 = 1000
     y2 = 1000
     X = (x1 + x2) / 2
     Y = (y1 + y2) / 2
-    return 'OK' #[X, Y]
+    return "OK"  # [X, Y]
+
 
 def PrecisePointingTelescope(ObsData):
     print("Pointing the telescope (precise) - wait 3s")
     time.sleep(3)
-    return 'OK'
+    return "OK"
+
 
 def ActivateAutoguiding(ObsData):
     print("Activate autoguiding (PHD2) - wait 3s")
     time.sleep(3)
-    return 'OK'
+    return "OK"
+
 
 def TakeTargetSpectraSeries(ObsData):
     nb = 3
     exptime = 2
     print(f"Take {nb} images of {exptime} seconds - wait 3s")
     print("... en fait, je ne prends qu'une image pour le moment")
-    print(ObsData['Devices'])
-    camera = ObsData['Devices']['ambiance_camera']
+    print(ObsData["Devices"])
+    camera = ObsData["Devices"]["ambiance_camera"]
     TakeImage(camera, "Ambiance.fits")
-    camera = ObsData['Devices']['science_camera']
+    camera = ObsData["Devices"]["science_camera"]
     TakeImage(camera, "Science.fits")
-    camera = ObsData['Devices']['guiding_camera']
+    camera = ObsData["Devices"]["guiding_camera"]
     TakeImage(camera, "Guiding.fits")
     # time.sleep(3)
-    return 'OK'
+    return "OK"
+
 
 def StopAutoguiding(ObsData):
     print("Stop autoguiding (PHD2) - wait 3s")
     time.sleep(3)
-    return 'OK'
+    return "OK"
+
 
 def TakeCalibSpectraSeries(ObsData):
     nb = 3
     exptime = 2
     print(f"Take {nb} calib images of {exptime} seconds - wait 3s")
     time.sleep(3)
-    return 'OK'
+    return "OK"
+
 
 def TakeFlatSpectraSeries(ObsData):
     nb = 3
     exptime = 2
     print(f"Take {nb} flat images of {exptime} seconds - wait 3s")
     time.sleep(3)
-    return 'OK'
+    return "OK"
+
 
 def TakeDarkSeries(ObsData):
     nb = 3
     exptime = 2
     print(f"Take {nb} dark images of {exptime} seconds - wait 3s")
     time.sleep(3)
-    return 'OK'
+    return "OK"
+
 
 def TakeBiasSeries(ObsData):
     nb = 3
     print(f"Take {nb} bias images - wait 3s")
     time.sleep(3)
-    return 'OK'
+    return "OK"
+
 
 def CreateObservationFile(ObsData):
     obsfilename = "TEST"
     print(f"Create the observation file: {obsfilename}.yaml - wait 3s")
     time.sleep(3)
-    return 'OK'
+    return "OK"
 
-#--------------------------
+
+# --------------------------
 # Temporary functions
-#--------------------------
+# --------------------------
+
 
 def TakeImage(ObsData, image_name):
-    print("Ho...") 
-    camID = ObsData['Devices']['science_camera']
+    print("Ho...")
+    camID = ObsData["Devices"]["science_camera"]
     if camID.is_connected:
         print("Ca va ")
         camID.prepare_shoot()
@@ -131,10 +149,11 @@ def TakeImage(ObsData, image_name):
         fitsIm = camID.get_received_image()
         print("Image reçue !")
         ImName = image_name or "TESTAEFFACER.fits"
-        fitsIm.writeto(ImName, overwrite=True) 
+        fitsIm.writeto(ImName, overwrite=True)
     else:
         print("Device pas connecté")
     return "OK"
+
 
 def F1(devices_list, ObsData):
     print("F1 - début")
@@ -142,13 +161,15 @@ def F1(devices_list, ObsData):
     print("F1 - fin")
     return "OK"
 
+
 def TakeOneImage(devices_list, ObsData):
     print("Acquisition - début")
     # time.sleep(3)
-    camera = devices_list['ScienceCam']
+    camera = devices_list["ScienceCam"]
     TakeImage(camera)
     print("Acquisition - fin")
     return "OK"
+
 
 def F3(devices_list, ObsData):
     print("F3 - début")
@@ -156,11 +177,13 @@ def F3(devices_list, ObsData):
     print("F3 - fin")
     return "OK"
 
+
 def F4(devices_list, ObsData):
     print("F4 - début")
     time.sleep(3)
     print("F4 - fin")
     return "OK"
+
 
 def F5(devices_list, ObsData):
     print("F5 - début")
